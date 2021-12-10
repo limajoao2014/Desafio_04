4de10cffbbe076e753e91d387d7f99c6cd24e3a4
Joao Miguel <limajoao2014@outlook.com.br>
2021-12-10 00:00:26 -0300

    desafio 4 concluido

---
+++ desafio04/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio04
+{
+    public class Aluno
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+        public double nota { get; set; }
+    }
+}
---
+++ desafio04/Program.cs
@@ -0,0 +1,44 @@
+﻿using desafio04;
+double soma=0;
+Random randNum = new Random();
+Aluno[] aluno = new Aluno[3];
+aluno[0] = new Aluno();
+aluno[1] = new Aluno();
+aluno[2] = new Aluno();
+
+List<string>randNames=new List<string>();
+randNames.Add("israel");
+randNames.Add("Orlando");
+randNames.Add("Miguel");
+
+var arrayNome= new string[3];
+int [] arrayIdade = new int[3];
+double [] arrayNota = new double[3];
+
+for (int i = 0; i < 3; i++) {
+    arrayNota[i] = randNum.Next(0,10);
+    arrayIdade[i] = randNum.Next(0,70);
+    arrayNome[i] = randNames[i];
+
+}
+
+for (int i = 0; i < 3; i++)
+{
+    aluno[i].nome = arrayNome[i];
+    aluno[i].nota = arrayNota[i];
+    aluno[i].idade = arrayIdade[i];
+
+}
+List<Aluno>alunos=new List<Aluno>();
+
+for (int i = 0; i < 3; i++)
+{
+    alunos.Add(aluno[i]);
+    soma = soma + alunos[i].nota;
+    Console.WriteLine(alunos[i].nome + " " + aluno[i].nota);  
+}
+        
+        Console.WriteLine("A soma de todas as notas é " + soma);
+
+
+Console.ReadKey();  
